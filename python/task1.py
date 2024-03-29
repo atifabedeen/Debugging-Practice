367d1123821d2be5a2e7d115087f50a5711e46c0
nekomi2 <dore123222@gmail.com>
2024-03-29 12:38:08 -0400

    fix task

--- python/task1.py
+++ python/task1.py
@@ -1,10 +1,11 @@
 def create_2d_list():
-    arr = [[] * 3] * 3
-    for i in range(3):
-        for j in range(3):
-            arr[i][j] = i * j
-    assert arr[2][2] == 4
-
+    arr = [[0] * 3] * 3
+    arr[0][0] = 1
+    arr[1][0] = 2
+    arr[2][0] = 3
+    assert arr[0][0] == 1
+    assert arr[1][0] == 2
+    assert arr[2][0] == 3
 
 if __name__ == "__main__":
     create_2d_list()
