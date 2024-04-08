eecc3183902d629452b85277a3900ed8e6523588
Mohammadreza Daneshvaramoli <daneshvaramoli@ce.sharif.edu>
2024-04-08 17:03:48 -0400

    Create Hanoi.java

---
+++ Java/Hanoi.java
@@ -0,0 +1,53 @@
+public class Hanoi {
+
+	public static void main(String[] args) {
+		hanoi1HardCoded("A", "C");
+	}
+	
+	public static void hanoi1HardCoded(String start, String end) {
+		System.out.println(start + " -> " + end);		
+	}
+	
+	public static void hanoi2HardCoded(String start, String spare, String end) {
+		System.out.println(start + " -> " + spare);		
+		System.out.println(start + " -> " + end);		
+		System.out.println(spare + " -> " + end);		
+	}
+	
+	public static void hanoi3HardCoded(String start, String spare, String end) {
+		System.out.println(start + " -> " + end);		
+		System.out.println(start + " -> " + spare);		
+		System.out.println(end + " -> " + spare);		
+		System.out.println(start + " -> " + end);		
+		System.out.println(spare + " -> " + start);		
+		System.out.println(spare + " -> " + end);		
+		System.out.println(start + " -> " + end);		
+	}
+	
+	public static void hanoi1CallingSimplerMethods(String start, String end) {
+		System.out.println(start + " -> " + end);		
+	}
+	
+	public static void hanoi2CallingSimplerMethods(String start, String spare, String end) {
+		hanoi1CallingSimplerMethods(start, spare);		
+		System.out.println(start + " -> " + end);		
+		hanoi1CallingSimplerMethods(spare, end);		
+	}
+	
+	public static void hanoi3CallingSimplerMethods(String start, String spare, String end) {
+		hanoi2CallingSimplerMethods(start, end, spare);		
+		System.out.println(start + " -> " + end);		
+		hanoi2CallingSimplerMethods(spare, start, end);		
+	}
+
+	public static void hanoi(String start, String spare, String end, int n) {
+		if (n == 1) {
+			System.out.println(start + " -> " + end);
+		} else {
+			hanoi(start, end, spare, n - 1);
+			System.out.println(start + " -> " + end);
+			hanoi(spare, start, end, n - 1);			
+		}
+	}
+
+}
